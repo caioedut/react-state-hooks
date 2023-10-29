272d96021eceeed31a56d6f435ed6f735f3e66ed
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-10-29 17:14:11 -0300

    chore: exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as useListState } from './useListState';
 export { default as useNumberState } from './useNumberState';
 export { default as useObjectState } from './useObjectState';
 export { default as usePropState } from './usePropState';
+export { default as useStoreState } from './useStoreState';
 export { default as useToggleState } from './useToggleState';
